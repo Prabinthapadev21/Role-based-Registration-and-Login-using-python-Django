e3740cb3247baa0f3573c05ca154b4e42838174d
Prabinthapadev21 <codinghub57@gmail.com>
2025-12-14 20:57:03 +0545

    updated..

--- Role_based_login_system/Role_based_login_system/settings.py
+++ Role_based_login_system/Role_based_login_system/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-)25v(r1j%r59j(w%cd72^@!7(qs*#4o1lf-li!l=1&0*74brek
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -73,6 +75,16 @@ WSGI_APPLICATION = 'Role_based_login_system.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = config('SECRET_KEY')
+
+ALLOWED_HOSTS = ['127.0.0.1', 'role-based-login-system.onrender.com']
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 DATABASES = {
     'default': {
@@ -117,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
